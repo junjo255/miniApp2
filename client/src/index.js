0966d517946da459dcfe548cdd59c8128b14ade6
junjo255 <jun.jo249@gmail.com>
2020-04-26 16:01:42 -0400

    refactor

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './App';
+
+const App = () => {
+
+    /**
+     * TODO
+     */
+
+}
 
 render(<App />, document.getElementById('app'));
