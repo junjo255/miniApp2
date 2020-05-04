6266f6e9f2f0e504638c12df16553e76465b5429
David Yoon <david_yoon86@hotmail.com>
2020-05-03 23:16:45 -0400

    mvp

---
+++ client/src/components/caseContainer.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { render } from 'react-dom';
+import CaseRow from './caseRow.js';
+
+
+const CaseContainer = (props) => {
+	return (
+		<table>
+      <thead>
+        <tr>
+          <th>Rank</th>
+          <th>Zipcode</th>
+          <th>Positive</th>
+          <th>Total</th>
+        </tr>
+      </thead>
+      <tbody>
+      	{
+      		props.cases.map((covidCase, idx) => {
+      			return (
+      				<CaseRow key={idx} covidCase={covidCase} idx={idx + 1} />
+      				)
+      		})
+      	}
+      </tbody>
+    </table>	
+
+		)
+}
+
+export default CaseContainer;
--- server/index.js
+++ server/index.js
@@ -14,39 +14,37 @@ app.use(cors());
 
 // ROUTES
 app.use('/api/covid', (req, res) => {
-  const API_URI = 'https://github.com/nychealth/coronavirus-data/blame/d34e6aab1e0dd0e0125e74519489e7893d33c9dd/tests-by-zcta.csv'
-  axios(API_URI)
-      .then(response => {
-        const html = response.data;
-        const $ = cheerio.load(html);
-        const statsTable = $('.width-full > div');
-        const covidByZipCode = [];
-        let id = 1
+    const API_URI = 'https://github.com/nychealth/coronavirus-data/blame/d34e6aab1e0dd0e0125e74519489e7893d33c9dd/tests-by-zcta.csv'
+    axios(API_URI)
+        .then(response => {
+            const html = response.data;
+            const $ = cheerio.load(html);
+            const statsTable = $('.width-full > div');
+            const covidByZipCode = [];
+            let id = 1
 
-        statsTable.each(() => {
+            statsTable.each(function() {
 
-          const zip_code = $(this).find('.js-file-line').text().split(',')[0];
-          const pos = $(this).find('.js-file-line').text().split(',')[1];
-          const tot = $(this).find('.js-file-line').text().split(',')[2];
+                const zip_code = $(this).find('.js-file-line').text().split(',')[0];
+                const pos = $(this).find('.js-file-line').text().split(',')[1];
+                const tot = $(this).find('.js-file-line').text().split(',')[2];
 
-          if (zip_code !== "" && zip_code !== "MODZCTA" && zip_code !== "NA") {
-            covidByZipCode.push({
-              "id": id,
-              "zipCode": zip_code,
-              "positive": pos,
-              "total": tot
+                if (zip_code !== "" && zip_code !== "MODZCTA" && zip_code !== "NA") {
+                    covidByZipCode.push({
+                        "id": id,
+                        "zipCode": zip_code,
+                        "positive": pos,
+                        "total": tot
+                    })
+                    id++;
+                }
             })
-            id++;
-          }
+            res.status(200).send(JSON.stringify(covidByZipCode));
         })
-
-          console.log(covidByZipCode);
-          res.status(200).send(JSON.stringify(covidByZipCode));
-      })
 });
 
 app.get('*', (_, res) => res.status(404).send({ message: 'Page not found.' }));
 
-app.listen(port, () => {
+app.listen(port, function () {
     console.log(`listening on port ${port}`);
 })
